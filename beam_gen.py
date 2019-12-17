f68fdd0db3f037828e294a722f52d6988cbb01ed
Ponomarev Sergei <papasergiusv@gmail.com>
2019-12-17 23:02:15 +0300

    Add params for training

---
+++ beam_gen.py
@@ -0,0 +1,8 @@
+import sys
+
+if len(sys.argv)<=2:
+    print("write arguments!")
+    exit(1)
+
+_model_name = sys.argv[1]
+_text = sys.argv[2]
---
+++ train.py
@@ -0,0 +1,185 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import youtokentome as yttm
+
+import libs
+from libs.data import tokenize_corpus, build_vocabulary, save_texts_to_file, LanguageModelDataset, load_chunks, BeamGenerator
+from libs.pipeline import train_eval_loop, init_random_seed
+from libs.base import get_params_number
+
+import sys
+
+if len(sys.argv)<=5:
+    print("write arguments!")
+    exit(1)
+
+_text = sys.argv[1]
+_text_train = sys.argv[2]
+_model_name = sys.argv[3]
+_batch_size = sys.argv[4]
+_epoch_count = sys.argv[5]
+
+init_random_seed()
+
+all_chunks = load_chunks("./texts/"+_text, 300)
+
+print("Chunks count: ", len(all_chunks))
+
+np.random.shuffle(all_chunks)
+
+TRAIN_SPLIT = int(len(all_chunks) * 0.7)
+train_texts = all_chunks[:TRAIN_SPLIT]
+test_texts = all_chunks[TRAIN_SPLIT:]
+
+print("Learn size:", len(train_texts))
+print("Valid size:", len(test_texts))
+
+BPE_MODEL_FILENAME = './models/kpss.yttm'
+TRAIN_TEXTS_FILENAME = './texts/'+_text_train
+save_texts_to_file(train_texts, TRAIN_TEXTS_FILENAME)
+yttm.BPE.train(data=TRAIN_TEXTS_FILENAME, vocab_size=1000, model=BPE_MODEL_FILENAME)
+tokenizer = yttm.BPE(BPE_MODEL_FILENAME)
+
+print("TOKENS:")
+print(' '.join(tokenizer.vocab()))
+
+train_token_ids = tokenizer.encode(train_texts, bos=True, eos=True)
+test_token_ids = tokenizer.encode(test_texts, bos=True, eos=True)
+token_counts = np.bincount([token_id for text in train_token_ids for token_id in text])
+
+unknown_subwords_in_test = sum(1 for text in test_token_ids for token_id in text if token_id == 1)
+print("Count unknown n-gramms:", unknown_subwords_in_test)
+
+
+CHUNK_LENGTH = 80
+
+train_dataset = LanguageModelDataset(train_token_ids, chunk_length=CHUNK_LENGTH)
+test_dataset = LanguageModelDataset(test_token_ids, chunk_length=CHUNK_LENGTH)
+
+train_dataset[0]
+
+tokenizer.decode(list(train_dataset[0]))
+
+def make_target_dependency_mask(length):
+    full_mask = torch.ones(length, length)
+    ignore_mask = torch.tril(full_mask) < 1
+    full_mask.masked_fill_(ignore_mask, float('-inf'))
+    full_mask.masked_fill_(~ignore_mask, 0)
+    return full_mask
+
+make_target_dependency_mask(10)
+
+
+def make_positional_encoding(max_length, embedding_size):
+    time = np.pi * torch.arange(0, max_length).float()
+    freq_dividers = torch.arange(1, embedding_size // 2 + 1).float()
+    inputs = time[:, None] / freq_dividers[None, :]
+    
+    result = torch.zeros(max_length, embedding_size)
+    result[:, 0::2] = torch.sin(inputs)
+    result[:, 1::2] = torch.cos(inputs)
+    return result
+
+sample_pos_codes = make_positional_encoding(30, 30)
+
+class LanguageModel(nn.Module):
+    def __init__(self, vocab_size, embedding_size, backbone, emb_dropout=0.0):
+        super().__init__()
+        self.embedding_size = embedding_size
+        self.embeddings = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
+        self.emb_dropout = nn.Dropout(emb_dropout)
+        self.backbone = backbone
+        self.out = nn.Linear(embedding_size, vocab_size)
+    
+    def forward(self, seed_token_ids):
+        """
+            seed_token_ids - BatchSize x MaxInLen
+        """
+        batch_size, max_in_length = seed_token_ids.shape
+
+        seed_padding_mask = seed_token_ids == 0
+        dependency_mask = make_target_dependency_mask(max_in_length) \
+            .to(seed_token_ids.device)
+        
+        seed_embs = self.embeddings(seed_token_ids) 
+        pos_codes = make_positional_encoding(max_in_length,
+                                             self.embedding_size).unsqueeze(0).to(seed_embs.device)
+        seed_embs = seed_embs + pos_codes
+        seed_embs = self.emb_dropout(seed_embs)
+
+        target_features = seed_embs
+        target_features = self.backbone(seed_embs,
+                                        mask=dependency_mask,
+                                        src_key_padding_mask=seed_padding_mask)
+        logits = self.out(target_features)
+        return logits
+
+def lm_cross_entropy(pred, target):
+    """
+    pred - BatchSize x TargetLen x VocabSize
+    target - BatchSize x TargetLen
+    """
+    pred_flat = pred.view(-1, pred.shape[-1])
+    target_flat = target.view(-1)
+    return F.cross_entropy(pred_flat, target_flat, ignore_index=0)
+
+
+def lr_scheduler(optimizer):
+    return torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                      patience=20,
+                                                      factor=0.5,
+                                                      verbose=True)
+
+class BatchFirstTransformerEncoder(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.impl = nn.TransformerEncoder(*args, **kwargs)
+        self.initialize_weights()
+    
+    def forward(self, src, *args, **kwargs):
+        src = src.transpose(0, 1).contiguous()
+        result = self.impl(src, *args, **kwargs)
+        result = result.transpose(0, 1).contiguous()
+        return result
+    
+    def initialize_weights(self):
+        for param in self.impl.parameters():
+            if param.dim() > 1:
+                nn.init.xavier_uniform_(param)
+
+torch_transf_model = LanguageModel(tokenizer.vocab_size(),
+                                   256,
+                                   BatchFirstTransformerEncoder(
+                                       nn.TransformerEncoderLayer(
+                                           d_model=256,
+                                           nhead=16,
+                                           dim_feedforward=512,
+                                           dropout=0.1),
+                                       num_layers=3),
+                                   emb_dropout=0.1)
+print('Params count', get_params_number(torch_transf_model))
+
+
+(best_val_loss, best_torch_transf_model) = train_eval_loop(torch_transf_model,
+                                            train_dataset,
+                                            test_dataset,
+                                            lm_cross_entropy,
+                                            lr=2e-3,
+                                            epoch_n=_epoch_count,
+                                            batch_size=_batch_size,
+                                            device='cuda',
+                                            early_stopping_patience=50,
+                                            max_batches_per_epoch_train=1000,
+                                            max_batches_per_epoch_val=1000,
+                                            lr_scheduler_ctor=lr_scheduler)
+
+torch.save(best_torch_transf_model.state_dict(), './models/'+_model_name)
+
